6c411c164e0fc04c085ae697e99739cd210091aa
Marcello Bozzi <bozzimarcello@gmail.com>
2023-12-22 18:15:52 +0100

    completed project

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.firstColor, this.secondColor, {super.key});
+
+  final Color firstColor;
+  final Color secondColor;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [firstColor, secondColor],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
